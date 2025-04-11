317ab0f2c61f05c3a6925792635aa6c48e70ce02
Bianca <103384402+TokiLoshi@users.noreply.github.com>
2025-04-11 10:58:55 -0700

    adding new packages

--- src/components/canvas/MapboxExample.jsx
+++ src/components/canvas/MapboxExample.jsx
@@ -1,26 +1,97 @@
-// import React, { useEffect, useRef } from 'react'
+// 'use client'
+
+// import { useEffect, useRef } from 'react'
 // import mapboxgl from 'mapbox-gl'
 // import 'mapbox-gl/dist/mapbox-gl.css'
 
-// const MapboxExample = () => {
-//   const mapContainerRef = useRef()
-//   const mapRef = useRef()
-//   const mapBoxToken = process.env.MAPBOX_TOKEN
-//   useEffect(() => {
-//     mapboxgl.accessToken = mapBoxToken
-//     mapRef.current = new mapboxgl.Map({
-//       container: mapContainerRef.current,
-//       center: [-74.5, 40],
-//       zoom: 9, // starting zoom
-//     })
-//   })
-//   return <div style={{ height: '100' }} ref={mapContainerRef} className='map-container' />
-// }
+// export default function MapboxGlobe() {
+//   const mapContainer = useRef(null)
+//   const map = useRef(null)
 
-// export default MapboxExample
+//   useEffect(() => {
+//     if (map.current) return
+
+//     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN
+
+//     map.current = new mapboxgl.Map({
+//       container: mapContainer.current,
+//       style: 'mapbox://styles/mapbox/satellite-v9',
+//       projection: 'globe',
+//       zoom: 1.5,
+//       center: [0, 20],
+//     })
+//     map.current.addControl(new mapboxgl.NavigationControl())
+//     map.current.on('style.load', () => {
+//       map.current.setFog({
+//         color: 'rgb(186, 210, 235)',
+//         'high-color': 'rgb(36, 92, 223)',
+//         'horizon-blend': 0.02,
+//         'space-color': 'rgb(11, 11, 25)',
+//         'star-intensity': 0.6,
+//       })
+//     })
+//     return () => {
+//       if (map.current) {
+//         map.current.remove()
+//       }
+//     }
+//   }, [])
+//   return (
+//     <div className='map-container'>
+//       <div ref={mapContainer} style={{ width: '100%', height: '100vh' }} />
+//     </div>
+//   )
+// }
 
 'use client'
 
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-const MapboxExample = ({ mapboxToken }) => {}
+export default function MapboxGlobe() {
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+
+  useEffect(() => {
+    if (map.current) return
+
+    // Access the public environment variable
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN
+
+    // For debugging - to see if the token is available
+    console.log('Token available:', !!process.env.NEXT_PUBLIC_MAPBOX_TOKEN)
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/satellite-v9',
+      projection: 'globe',
+      zoom: 1.5,
+      center: [0, 20],
+    })
+
+    map.current.addControl(new mapboxgl.NavigationControl())
+
+    map.current.on('style.load', () => {
+      map.current.setFog({
+        color: 'rgb(186, 210, 235)',
+        'high-color': 'rgb(36, 92, 223)',
+        'horizon-blend': 0.02,
+        'space-color': 'rgb(11, 11, 25)',
+        'star-intensity': 0.6,
+      })
+    })
+
+    return () => {
+      if (map.current) {
+        map.current.remove()
+      }
+    }
+  }, [])
+
+  return (
+    <div className='map-container'>
+      <div ref={mapContainer} style={{ width: '100%', height: '100vh' }} />
+    </div>
+  )
+}
